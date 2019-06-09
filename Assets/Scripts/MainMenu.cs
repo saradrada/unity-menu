571d33a86d3e5983f40e2f8b221a51013e13d541
saradrada <saradrada@hotmail.com>
2019-06-09 16:23:14 -0500

    Racing buttons fully working on mode Training

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-
-    public void Salir()
+    public int contador = 1;
+    public Button button;
+    public void Salir() 
     {
         Application.Quit();
     }
 
-}
+   }
